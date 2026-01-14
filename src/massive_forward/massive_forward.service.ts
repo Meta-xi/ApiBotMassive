ff3fa705e3b3c1c67ff35fff2cb6f5c112e8216e
= <=>
2026-01-14 16:14:39 -0500

    probando con 10 grupos

--- src/massive_forward/massive_forward.service.ts
+++ src/massive_forward/massive_forward.service.ts
@@ -188,10 +188,10 @@ export class MassiveForwardService {
                     idsToSend = [idsToSend];
                 }
                 idsToSend = idsToSend.map(group => group.split(',')).flat();
-                if(idsToSend.length > 50){
-                    idsToSend.splice(50);
+                if(idsToSend.length > 10){
+                    idsToSend.splice(10);
                 }
-                
+
                 const idGroup = idsToSend.shift();
                 config.ids_destino = idsToSend.length > 0 ?[idsToSend.join(',')] : [];
                 Logger.log(idGroup);
